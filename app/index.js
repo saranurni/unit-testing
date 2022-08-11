9591b00850eb0c4c41775982984344a492c40b90
Sara Nurestani <saranurestani@gmail.com>
2022-08-11 20:09:48 +0100

    Test different parameters

--- app/index.js
+++ app/index.js
@@ -1,1 +1,5 @@
 export const add2Nums = (num1, num2) => num1 + num2;
+
+export const greet = (name = "Anonymous") => `Hello, ${name}!`;
+
+export const calculateTip = (total, tipRate = 20) => (total * tipRate) / 100;
--- app/index.test.js
+++ app/index.test.js
@@ -1,5 +1,29 @@
-import { add2Nums } from "./index.js";
+import { add2Nums, calculateTip, greet } from "./index.js";
 
-test("1 + 1 = 2", () => {
-  expect(add2Nums(1, 1)).toBe(2);
+test("5 + 6 = 11", () => {
+  expect(add2Nums(5, 6)).toBe(11);
+});
+
+describe("greet", () => {
+  // Happy path test
+  test("it greets with the correct name", () => {
+    expect(greet("Sara")).toBe("Hello, Sara!");
+    expect(greet("Jane")).toBe("Hello, Jane!");
+  });
+
+  // Sad path test
+  test("greets anonymous", () => {
+    expect(greet()).toBe("Hello, Anonymous!");
+  });
+});
+
+describe("calculateTip", () => {
+  it("calculates a $10 tip on a $100 tab at 10%", () => {
+    expect(calculateTip(100, 10)).toBe(10);
+    expect(calculateTip(100, 20)).toBe(20);
+  });
+
+  it("calculates with a default tip of 20%", () => {
+    expect(calculateTip(100)).toBe(20);
+  });
 });
